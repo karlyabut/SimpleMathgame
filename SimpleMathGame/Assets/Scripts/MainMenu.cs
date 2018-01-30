2d43cb5f33e52f22a7ebed5bdeb7f512004cfe22
karlyabut <karleisenyabut@gmail.com>
2018-01-30 17:57:26 -0500

    First Commit

---
+++ SimpleMathGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    private Transform cameraTransform;
+    private Transform cameraDesiredLookAt;
+
+	// Use this for initialization
+	private void Start ()
+    {
+        cameraTransform = Camera.main.transform;	
+	}
+
+    private void Update()
+    {
+        if(cameraDesiredLookAt != null)
+        {
+            cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, cameraDesiredLookAt.rotation, 3 * Time.deltaTime);
+        }
+    }
+	
+    public void LookAtMenu(Transform menuTransform)
+    {
+        cameraDesiredLookAt = menuTransform;
+    }
+
+}
